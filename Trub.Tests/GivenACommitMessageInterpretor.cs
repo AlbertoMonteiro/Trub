b10c400e3942de149e5af9d2ca2914e4bc655537
Alberto Monteiro <alberto.monteiro@live.com>
2014-06-25 13:17:48 -0300

    Created Trub.Tracker project and moved CommitMessageInterpretor to this project

--- Trub.Tests/GivenACommitMessageInterpretor.cs
+++ Trub.Tests/GivenACommitMessageInterpretor.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using SharpTestsEx;
-using TreHub.Models;
+using Trub.Tracker;
 
 namespace Trub.Tests
 {
---
+++ Trub.Tracker/ICommitMessageInterpretor.cs
@@ -0,0 +1,13 @@
+namespace Trub.Tracker
+{
+    public interface ICommitMessageInterpretor
+    {
+        CardAction Interpret(string commitMessage);
+    }
+
+    public class CardAction
+    {
+        public string CardId { get; set; }
+        public CardActionKind Kind { get; set; }
+    }
+}
